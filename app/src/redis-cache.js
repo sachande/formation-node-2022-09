2cefafbc3173d889ff28f1b232ee6d1df6c512c0
Nicolas Chambrier <naholyr@gmail.com>
2022-09-02 17:19:08 +0200

    day 3: cache stampede

---
+++ app/src/redis-cache.js
@@ -0,0 +1,72 @@
+import { client } from "./redis-client.js";
+
+const sleep = promisify(setTimeout);
+
+const stale = 3600000;
+
+const get = async (key) => {
+  const result = await client.get("cache:" + key); // null or value
+  return result;
+};
+
+const set = async (key, value, ttl = 600000) => {
+  await client.set("cache:" + key, value, "PX", ttl + stale);
+};
+
+const acquireLock = async (key, timeout = 60000) => {
+  const acquired = await client.setnx("lock:" + key, "");
+  if (acquired) {
+    await client.pexpire("lock:" + key, timeout);
+    return true;
+  }
+  return false;
+};
+
+const releaseLock = async (key) => {
+  const acquired = await client.setnx("lock:" + key, "");
+  return !!acquired;
+};
+
+// TODO PUB/SUB to notify cache updates
+const waitForCache = async (key) => {
+  const ttl = await client.ttl("cache:" + key);
+  if (ttl >= stale) {
+    // available
+    return get(key);
+  }
+  await sleep(100);
+  return await waitForCache(key);
+};
+
+const getOrCompute = async (key, fn) => {
+  const ttl = await client.ttl("cache:" + key);
+
+  if (ttl >= stale) {
+    // available
+    return get(key);
+  }
+
+  // try to acquire a lock
+  const acquired = await acquireLock(key);
+  if (!acquired && ttl < 0) {
+    return waitForCache(key); // TODO
+  }
+
+  if (!acquired) {
+    // return expired data
+    return get(key);
+  }
+
+  // acquired lock: let's compute
+  try {
+    const result = await fn();
+    await set(key, result);
+    return result;
+  } finally {
+    releaseLock(key);
+  }
+};
+
+const redisCache = { get, set, acquireLock, releaseLock, getOrCompute };
+
+export default redisCache;
--- app/workers/fibo-worker.js
+++ app/workers/fibo-worker.js
@@ -1,15 +1,42 @@
 import { Emitter } from "@socket.io/redis-emitter";
 import { Worker } from "bullmq";
 import { fibonacci } from "../src/fibonacci.js";
+import redisCache from "../src/redis-cache.js";
 import { client as redisClient } from "../src/redis-client.js";
 
 const ioEmitter = new Emitter(redisClient);
 
+/*
+
+is it cached ?
+  yes
+  no but here is your lock
+  no but here is expired data
+
+W1: fibo(42)
+  is it cached ? no but here is your lock
+    if get lock ? here is your lock
+      compute
+        write cache
+        unlock finally
+W2: fibo(42)
+  is it cached ? no
+    if get lock ? nope
+    wait for cache… // or get from expired cache
+      get from cache
+
+*/
+
 new Worker(
   "Fibo",
-  (job) => {
+  async (job) => {
     const { n, author = "anonymous" } = job.data;
-    const result = fibonacci(job.data.n);
+
+    const key = "fibo:" + n;
+    let result = await redisCache.getOrCompute(key, () => {
+      return fibonacci(job.data.n);
+    });
+
     ioEmitter.emit("received-message", {
       text: `fibo(${n}) = ${result}`,
       date: Date.now(),
